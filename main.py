9728c89406593ffe7bbcb9d4c3debcea7c8089e7
CappSec <110314671+CappSec@users.noreply.github.com>
2025-05-06 19:54:44 -0500

    update

--- main.py
+++ main.py
@@ -6,29 +6,23 @@ from player import Player
 def main():
 	
 	pygame.init()
-	
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	
 	clock = pygame.time.Clock()
 	player = Player(SCREEN_HEIGHT / 2, SCREEN_HEIGHT / 2)
 	dt = 0
 
-
-	running = True
-	while running:
+	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-			
+
+		player.update(dt)
+
 		screen.fill("black")
 		player.draw(screen)
 		pygame.display.flip()
-		
+	
 		dt = clock.tick(60) / 1000
 		
-	print("Starting Asteroids!")
-	
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
 if __name__ == "__main__":
 	main()
--- player.py
+++ player.py
@@ -18,3 +18,14 @@ def triangle(self):
 
 def draw(self, screen):
     pygame.draw.polygon(screen, "white", slef.triangle(), 2)
+
+def rotate(dt):
+    self.rotation += PLAYER_TURN_SPEED * dt
+
+def update(self, dt):
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_a]:
+        self.rotate(-dt)
+    if keys[pygame.K_d]:
+        self.rotate(dt)
